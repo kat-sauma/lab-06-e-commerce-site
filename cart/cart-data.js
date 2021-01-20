55b23c2724ab7dcf8c649214d5e241c6f858df74
Kat Sauma <katsauma@gmail.com>
2021-01-20 15:42:12 -0800

    cart data added in cart-data.js

--- cart/cart-data.js
+++ cart/cart-data.js
@@ -1,49 +1,29 @@
-export const clothesInventory = [
+export const cart = [
     {
         id: 1,
-        name: 'Floral Silk Top',
-        image: '../assets/blue-silk-top.jpg',
-        description: 'blue silk top with a pan collar',
-        category: 'vintage-tops',
-        size: 'Large',
+        quantity: 1,
         price: 60
     },
     {
         id: 2,
-        name: 'Denim Vest',
-        image: '../assets/denim-vest-velvet-collar.jpg',
-        description: 'denim vest with a black suede collar',
-        category: 'vintage-tops',
-        size: 'medium',
+        quantity: 1,
         price: 45,
     },
     {
         id: 3,
-        name: 'Burberry Ruffle Collared Tee',
-        image: '../assets/burberry-black-top.jpg',
-        description: 'black cotton polo with a ruffled collar',
-        category: 'vintage-tops',
-        size: 'small',
+        quantity: 1,
         price: 60,
     },
 
     {
         id: 4,
-        name: 'Beige Suede Button Down',
-        image: '../assets/suede-button-down-top.jpg',
-        description: 'beige, patterned, suede button down tee shirt with structure',
-        category: 'vintage-tops',
-        size: 'small',
+        quantity: 1,
         price: 30,
     },
 
     {
         id: 5,
-        name: 'Purple Stripe Tank',
-        image: '../assets/purple-stripe-tank.jpg',
-        description: 'lavender and mint striped spaghetti strap tank with diamond point hem',
-        category: 'vintage-tops',
-        size: 'medium',
+        quantity: 1,
         price: 20,
     }
 ];
