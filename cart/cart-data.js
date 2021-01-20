ac1321d44b91a8dd77fcfe27b8c736686413402e
Kat Sauma <katsauma@gmail.com>
2021-01-20 15:11:25 -0800

    design cart with table, set up pages

---
+++ cart/cart-data.js
@@ -0,0 +1,49 @@
+export const clothesInventory = [
+    {
+        id: 1,
+        name: 'Floral Silk Top',
+        image: '../assets/blue-silk-top.jpg',
+        description: 'blue silk top with a pan collar',
+        category: 'vintage-tops',
+        size: 'Large',
+        price: 60
+    },
+    {
+        id: 2,
+        name: 'Denim Vest',
+        image: '../assets/denim-vest-velvet-collar.jpg',
+        description: 'denim vest with a black suede collar',
+        category: 'vintage-tops',
+        size: 'medium',
+        price: 45,
+    },
+    {
+        id: 3,
+        name: 'Burberry Ruffle Collared Tee',
+        image: '../assets/burberry-black-top.jpg',
+        description: 'black cotton polo with a ruffled collar',
+        category: 'vintage-tops',
+        size: 'small',
+        price: 60,
+    },
+
+    {
+        id: 4,
+        name: 'Beige Suede Button Down',
+        image: '../assets/suede-button-down-top.jpg',
+        description: 'beige, patterned, suede button down tee shirt with structure',
+        category: 'vintage-tops',
+        size: 'small',
+        price: 30,
+    },
+
+    {
+        id: 5,
+        name: 'Purple Stripe Tank',
+        image: '../assets/purple-stripe-tank.jpg',
+        description: 'lavender and mint striped spaghetti strap tank with diamond point hem',
+        category: 'vintage-tops',
+        size: 'medium',
+        price: 20,
+    }
+];
---
+++ cart/readme.md
@@ -0,0 +1,20 @@
+Data Modeling:
+SHOPPING CART
+1) id
+    product that this line item refers to
+2) quantity
+    how many of the product are being ordered
+3) price
+    how much a product costs
+4)total
+    price * quantity
+
+
+1)for loop access iten in array with id
+2) write, test and use function taking array and id
+    return item in array with matching id
+3) write, test, use function sopping cart and calculates the total price of one item based on quantity
+4)write, test, use fxn takes an array of shopping cart items
+    calculate total price 
+5) cart items an products-- two relational data models
+
