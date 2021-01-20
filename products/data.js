216f88b2743b3e7e862d7c12f931039bcac56997
Kat Sauma <katsauma@gmail.com>
2021-01-19 17:55:08 -0800

    data.js array created with object literals for each product stored in variable clothing

--- products/data.js
+++ products/data.js
@@ -4,51 +4,46 @@ export const clothing = [
         name: 'blue-silk-top',
         image: 'assets/blue-silk-top.jpg',
         description: 'blue silk top with a pan collar',
-        category: 'vintage tops',
+        category: 'vintage-tops',
         size: 'large',
         price: 60
     },
-    // {
-    //     id: ,
-    //     name: ,
-    //     image: ,
-    //     description: ,
-    //     category: ,
-    //     price: ,
-    // },
-    // {
-    //     id: ,
-    //     name: ,
-    //     image: ,
-    //     description: ,
-    //     category: ,
-    //     price: ,
-    // },
-    // 
-    // {
-    //     id: ,
-    //     name: ,
-    //     image: ,
-    //     description: ,
-    //     category: ,
-    //     price: ,
-    // },
-    // 
-    // {
-    //     id: ,
-    //     name: ,
-    //     image: ,
-    //     description: ,
-    //     category: ,
-    //     price: ,
-    // },
-    // 
-    // {
-    //     id: ,
-    //     name: ,
-    //     image: ,
-    //     description: ,
-    //     category: ,
-    //     price: ,
-    // },
+    {
+        id: 2,
+        name: 'denim-vest',
+        image: 'assets/denim-vest-velvet-collar.jpg',
+        description: 'denim vest with a black suede collar',
+        category: 'vintage-tops',
+        size: 'medium',
+        price: 45,
+    },
+    {
+        id: 3,
+        name: 'burberry-black-tee',
+        image: 'assets/burberry-black-top.jpg',
+        description: 'black cotton polo with a ruffled collar',
+        category: 'vintage-tops',
+        size: 'small'
+        price: 60,
+    },
+
+    {
+        id: 4,
+        name: 'beige-suede-button-down',
+        image: 'assets/suede-button-down-top.jpg',
+        description: 'beige, patterned, suede button down tee shirt with structure',
+        category: 'vintage-tops',
+        price: 30,
+    },
+
+    {
+        id: 5,
+        name: 'purple-stripe-tank',
+        image: 'assets/purple-stripe-tank.jpg',
+        description: 'lavender and mint striped spaghetti strap tank with diamond point hem',
+        category: 'vintage-tops',
+        price: 20,
+    },
+
+    {
 ]
