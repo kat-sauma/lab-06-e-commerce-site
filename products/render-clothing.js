e4d67db2297f05b7278080a715b4d0c468fcc394
Kat Sauma <katsauma@gmail.com>
2021-01-21 22:07:50 -0800

    a LOT of CSS happened.

--- products/render-clothing.js
+++ products/render-clothing.js
@@ -2,22 +2,28 @@ export function renderClothing(clothing) {
     const li = document.createElement('li');
     li.classList.add('item-list');
 
+    const line = document.createElement('hr')
+    li.append(line);
+
     const itemId = document.createElement('p');
     itemId.classList.add('id');
     itemId.textContent = `${clothing.id}`;
     li.append(itemId);
 
-    const img = document.createElement('img');
-    img.classList.add('image');
-    li.append(img);
-    img.src = `${clothing.image}`;
-    img.setAttribute('height', 200);
-    img.setAttribute('width', 200);
-
     const h3 = document.createElement('h3');
     h3.classList.add('name');
     h3.textContent = clothing.name;
     li.append(h3);
+
+    const space = document.createElement('br');
+    li.append(space);
+
+    const img = document.createElement('img');
+    img.classList.add('image');
+    li.append(img);
+    img.src = `${clothing.image}`;
+    img.setAttribute('height', 300);
+    img.setAttribute('width', 300);
 
     const pPrice = document.createElement('p');
     pPrice.classList.add('price');
@@ -36,7 +42,7 @@ export function renderClothing(clothing) {
 
     const button1 = document.createElement('button');
     button1.classList.add('button');
-    button1.textContent = 'Add To Cart';
+    button1.textContent = 'mine';
     li.append(button1);
 
     return li;
